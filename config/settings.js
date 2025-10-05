05ea765b4e4a79d455ae73b8c6b397bb5d899d8a
MarcLeGuyader <Marc@maello.fr>
2025-10-05 03:22:24 +0200

    [clean] remove unused

--- config/settings.js
+++
@@ -1,6 +0,0 @@
-export const settings = {
-  debug: true,
-  owners: ["Marc","Sam","Sven"],
-  clients: ["ResQuant","PUFsecurity","Aico","eShard","IOTR"],
-  steps: ["Discovery","Qualified","Solution selling","Negotiation","Closing","Won","Lost"]
-};
